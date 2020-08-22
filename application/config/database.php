df62e8c4905c4d118d3bc1566a3016a847ba45b2
alfianlensun <alfianricky46@gmail.com>
2020-08-22 16:20:58 +0800

    git

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'db',
+	'hostname' => '127.0.0.1',
 	'username' => 'root',
-	'password' => 'root',
+	'password' => 'siksaskali',
 	'database' => 'absensi',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
